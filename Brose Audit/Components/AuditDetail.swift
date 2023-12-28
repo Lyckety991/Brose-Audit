bf6a43981d2163bd2f5fc035e16d0c92e9535fb6
Patrick Lanham <p.lanham91@icloud.com>
2023-12-28 14:54:03 +0100

    New Update 28.12.23

---
+++ Brose Audit/Components/AuditDetail.swift
@@ -0,0 +1,18 @@
+//
+//  AuditDetail.swift
+//  Brose Audit
+//
+//  Created by Patrick Lanham on 27.12.23.
+//
+
+import SwiftUI
+
+struct AuditDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuditDetail()
+}
